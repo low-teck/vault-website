ac8654435408c426019669a2cef08dcc8ad4656a
Tanmay Gairola <55156702+gairTanm@users.noreply.github.com>
2022-02-07 12:13:33 +0530

    layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,21 +43,25 @@ const Home: NextPage = () => {
 
 	return (
 		<div className="flex flex-col">
-			<section className="w-full h-screen">
-				<div className=" bg-hero-pattern bg-center bg-no-repeat bg-cover h-full w-full">
-					<div className="flex h-full justify-center w-8/12">
-						<div className="flex  flex-col px-10 mt-20 sm:mx-20 ml-30">
-							<h1 className="lg:text-8xl text-2xl font-lato mt-20 ">
-								Secure your files,{" "}
+			<section className="w-screen h-screen">
+				<div className="bg-hero-pattern bg-center bg-no-repeat bg-cover h-full w-full">
+					<div className="flex text-center justify-center h-full w-full">
+						<div className="flex justify-evenly w-8/12  flex-col px-10  ">
+							<h1 className="lg:text-8xl text-2xl font-lato  ">
+								Secure your files,
+								<br />
 								<span className="text-primary uppercase">
 									Vault!
 								</span>
 							</h1>
-							<h3 className="lg:text-2xl text-lg text-gray-dark w-90 mt-4">
+							<h3 className="lg:text-2xl text-lg text-gray-dark w-90 ">
 								A cross-platform, offline and secure app to
 								encrypt your files using strong cryptographic
 								algorithms.
 							</h3>
+							<button onClick={downloadApp}>
+								Download for {os}
+							</button>
 						</div>
 						{/*<div className="flex align-left sm:w-500 sm:h-500">
 							<Image src={heroImage} alt="hero image" />
